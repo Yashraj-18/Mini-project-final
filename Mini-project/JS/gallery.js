19321a607103a2d49a5aae30d39c08dae4790ad8
Yashraj-18 <113794165+Yashraj-18@users.noreply.github.com>
2023-01-30 08:41:41 +0530

    Add files via upload

---
+++ Mini-project/JS/gallery.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.list').click(function(){
+    const value = $(this).attr('data-filler');
+    if(value == 'all'){
+      $('.itemBox').show('1000');
+    }
+    else{
+      $('.itemBox').not('.' +value).hide('1000');
+      $('.itemBox').filler('.' +value).show('1000');
+
+    }
+  })
+})
